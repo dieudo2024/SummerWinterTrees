606074b912d4520fa11ba294141f838807ec8a8c
Dieudonné Bondo <dieudobond@gmail.com>
2025-12-29 22:51:57 -0500

    Adjust tree rendering parameters

--- src/SummerTreeAbstract.java
+++ src/SummerTreeAbstract.java
@@ -119,7 +119,7 @@ public abstract class SummerTreeAbstract extends JFrame implements KeyListener {
         }
 
         //double baseLength = Math.min(height, width) * 0.3;
-        drawTreeRec(width / 2.0, height - 20.0, -computePi(100) / 2.0, 90, 15,
+        drawTreeRec(width / 2.0, height - 20.0, -computePi(100) / 2.0, 80, 15,
                 new Color(155, 100, 60), depth);
     }
 
@@ -139,7 +139,7 @@ public abstract class SummerTreeAbstract extends JFrame implements KeyListener {
     }
 
     protected int getMaxDepth() {
-        return isWinterMode ? 10 : 30;
+        return isWinterMode ? 10 : 10;
     }
 
     protected void enforceDepthBounds() {
